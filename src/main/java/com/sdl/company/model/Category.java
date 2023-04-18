d9da8fc80fb707cb2a93d18ccac1016eb46ea1cc
Sergio De Luca <serg.r.deluca@gmail.com>
2023-04-18 19:13:49 -0300

    creacion entidad category

---
+++ src/main/java/com/sdl/company/model/Category.java
@@ -0,0 +1,32 @@
+package com.sdl.company.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name="category")
+public class Category implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String description;
+	
+	
+	
+	
+}
